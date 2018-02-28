02727df009b6e1c51d55c778e4fb9154283e1e14
liwangGT <wangliinhust@gmail.com>
2018-02-28 17:16:56 -0500

    updated training and validation sample quantity

--- model.py
+++ model.py
@@ -1,21 +1,6 @@
 import os
 import csv
 import datetime
-
-samples = []
-with open('Sample_data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        # skip the first line, which is the title line
-        if line[0] != 'center':
-            samples.append(line)
-        # for testing implementation only, need to be removed
-        #if len(samples)>100:
-        #    break
-
-from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
 import cv2
 import numpy as np
 import sklearn
@@ -23,9 +8,57 @@ from keras.models import Sequential, Model
 from keras.layers import Lambda, Input, Flatten, Dense, Cropping2D, Convolution2D
 from keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-# use generator to avoid large memory consumption
+def genModel():
+    """
+    generate CNN model (based on NVIDIA self driving car)
+    """
+    #ch, row, col = 3, 80, 320  # Trimmed image format
+    inp = (160, 320, 3)
+    oup = (80, 320, 3)
+
+    model = Sequential()
+    # crop top 50 pixels, bottom 30 pixels, left/right 0 pixels
+    model.add(Cropping2D(cropping=((50,30), (0,0)), input_shape=inp))
+    # Preprocess incoming data, centered around zero with small standard deviation 
+    model.add(Lambda(lambda x: x/127.5 - 1., input_shape=inp, output_shape=oup))
+    model.add(Convolution2D(24,5,5,subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(48,5,5,subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(64,3,3, activation="relu"))
+    model.add(Convolution2D(64,3,3, activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    return model
+
+def Plot_loss(history_object):
+    """
+    print history data and plot losses
+    """    
+    ### print the keys contained in the history object
+    print(history_object.history.keys())
+    print(history_object.history['loss'])
+    print(history_object.history['val_loss'])
+
+    ### plot the training and validation loss for each epoch
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('model mean squared error loss')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.show()
+
+
+
 def generator(samples, batch_size=32):
+    """
+    use generator to avoid large memory consumption
+    """
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
@@ -66,62 +99,65 @@ def generator(samples, batch_size=32):
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+def loadRawData():
+    """
+    load Raw data for steering angle
+    """
+    samples = []
+    with open('Sample_data/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            # skip the first line, which is the title line
+            if line[0] != 'center':
+                samples.append(line)
+            # for testing implementation only, need to be removed
+            #if len(samples)>100:
+            #    break
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+    return train_samples, validation_samples
 
-#ch, row, col = 3, 80, 320  # Trimmed image format
-inp = (160, 320, 3)
-oup = (80, 320, 3)
-
-model = Sequential()
-# crop top 50 pixels, bottom 30 pixels, left/right 0 pixels
-model.add(Cropping2D(cropping=((50,30), (0,0)), input_shape=inp))
-# Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=inp,
-        output_shape=oup))
-model.add(Convolution2D(24,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2), activation="relu"))
-model.add(Convolution2D(64,3,3, activation="relu"))
-model.add(Convolution2D(64,3,3, activation="relu"))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
-
-
-# model checkpoint
-now = datetime.datetime.now()
-datenow = now.strftime("%Y-%m-%d-")
-#file_path_model = "Model_checkpoints/" + datenow + "model-weights-{epoch:02d}-{val_loss:0.2f}.hdf5"
-file_path_model = "Model_checkpoints/" + datenow + "model-weights.hdf5"
-checkpoint = ModelCheckpoint(file_path_model, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
-callbacks_list = [checkpoint]
-
-model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, samples_per_epoch= \
-            len(train_samples), validation_data=validation_generator, \
-            nb_val_samples=len(validation_samples), nb_epoch=10, \
-            callbacks = callbacks_list, verbose=1)
-#history_object = model.fit_generator(train_generator, steps_per_epoch= len(train_samples), \
-#                 validation_data=validation_generator, validation_steps=len(validation_samples), \
-#                 callbacks = callbacks_list, epochs=5, verbose = 1)
+def trainModel(model, train_raw, validation_raw):
+    """
+    training data and save model checkpoints
+    """
+    # compile and train the model using the generator function
+    train_generator = generator(train_raw, batch_size=32)
+    validation_generator = generator(validation_raw, batch_size=32)
 
 
-### print the keys contained in the history object
-print(history_object.history.keys())
-print(history_object.history['loss'])
-print(history_object.history['val_loss'])
+    # model checkpoint
+    now = datetime.datetime.now()
+    datenow = now.strftime("%Y-%m-%d-")
+    #file_path_model = "Model_checkpoints/" + datenow + "model-weights-{epoch:02d}-{val_loss:0.2f}.hdf5"
+    file_path_model = "Model_checkpoints/" + datenow + "model-weights.hdf5"
+    checkpoint = ModelCheckpoint(file_path_model, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+    callbacks_list = [checkpoint]
+    model.compile(loss='mse', optimizer='adam')
+    # left/center/right images, and all flipped
+    ntrain = len(train_raw)*3*2
+    nvalid = len(validation_raw)*3*2
+    history_object = model.fit_generator(train_generator, samples_per_epoch= \
+                ntrain, validation_data=validation_generator, \
+                nb_val_samples=nvalid, nb_epoch=10, \
+                callbacks = callbacks_list, verbose=1)
+    #history_object = model.fit_generator(train_generator, steps_per_epoch= ntrain, \
+    #                 validation_data=validation_generator, validation_steps=nvalid, \
+    #                 callbacks = callbacks_list, epochs=5, verbose = 1)    
+    return history_object
 
-### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
 
+
+
+
+if __name__ == "__main__":
+    # load raw training data
+    train_raw, validation_raw = loadRawData()
+
+    # generate CNN model
+    model = genModel()
+
+    # train CNN model
+    history_object = trainModel(model, train_raw, validation_raw)
+
+    # plot training data
+    Plot_loss(history_object)
