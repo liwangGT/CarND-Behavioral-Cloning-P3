72b25d87591bb49611bf49ae51a5d6a1b2f8ca3d
liwangGT <wangliinhust@gmail.com>
2018-02-27 23:52:01 -0500

    implemented NVIDA DL network

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ with open('Sample_data/driving_log.csv') as csvfile:
         # skip the first line, which is the title line
         if line[0] != 'center':
             samples.append(line)
+        # for testing implementation only, need to be removed
         if len(samples)>100:
             break
 
@@ -19,17 +20,17 @@ import cv2
 import numpy as np
 import sklearn
 from keras.models import Sequential, Model
-from keras.layers import Lambda, Input, Flatten, Dense
+from keras.layers import Lambda, Input, Flatten, Dense, Cropping2D, Convolution2D
 from keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 
+# use generator to avoid large memory consumption
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
             images = []
             angles = []
             for batch_sample in batch_samples:
@@ -39,6 +40,12 @@ def generator(samples, batch_size=32):
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
+                # flip image to augment data
+                #image_flipped = np.fliplr(center_image)
+                #angle_flipped = -center_angle
+                #images.append(image_flipped)
+                #angles.append(angle_flipped)
+                # use left and right camera to augment data
 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -50,16 +57,25 @@ train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 #ch, row, col = 3, 80, 320  # Trimmed image format
-ch, row, col = 3, 160, 320  # Trimmed image format
-
+inp = (160, 320, 3)
+oup = (80, 320, 3)
 
 model = Sequential()
-inp = (row, col, ch)
+# crop top 50 pixels, bottom 30 pixels, left/right 0 pixels
+model.add(Cropping2D(cropping=((50,30), (0,0)), input_shape=inp))
 # Preprocess incoming data, centered around zero with small standard deviation 
 model.add(Lambda(lambda x: x/127.5 - 1.,
         input_shape=inp,
-        output_shape=inp))
+        output_shape=oup))
+model.add(Convolution2D(24,5,5,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 
